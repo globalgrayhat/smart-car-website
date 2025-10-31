0f079e560297cef3c9c28dfd72835b1125c595f5
ძεՏκƦαʍ⸙ <deskram@mail.ru>
2025-10-31 10:46:44 +0300

    Initial commit: setup Smart Car Website project with React, Vite, Express, and WebRTC

--- server.ts
+++ server.ts
@@ -5,7 +5,8 @@ import { Server, Socket } from "socket.io";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
-
+import dotenv from "dotenv";
+dotenv.config();
 // ====== ESM dirname ======
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
